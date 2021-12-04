9c37ff7126d5cc5ef76d4a0fdcb6ba4ca2dcedad
RAREBEEF <drrobot409@gmail.com>
2021-12-04 18:27:07 +0900

    refresh 404 fix

--- src/components/MemoView.js
+++ src/components/MemoView.js
@@ -2,34 +2,34 @@ import classNames from "classnames";
 import { useParams } from "react-router";
 import styles from "./MemoView.module.scss";
 import Toolbar from "./Toolbar";
-import MemoList from "./MemoList";
+// import MemoList from "./MemoList";
 
 export default function MemoView(props) {
   const id = useParams().id;
-  if (!JSON.parse(localStorage.getItem(id))) {
-    return (
-      <div>
-        <MemoList />
-      </div>
-    );
-  } else {
-    return (
-      <div className={classNames(styles["container"])}>
-        <Toolbar />
-        <div className={classNames(styles["section-text"])}>
-          <div className={classNames(styles["memo--header"])}>
-            <h2 className={classNames(styles["title"])}>
-              {JSON.parse(localStorage.getItem(id)).title}
-            </h2>
-            <div className={classNames(styles["time"])}>
-              {JSON.parse(localStorage.getItem(id)).time}
-            </div>
-          </div>
-          <div className={classNames(styles["memo--main-text"])}>
-            {JSON.parse(localStorage.getItem(id)).memo}
+  // if (!JSON.parse(localStorage.getItem(id))) {
+  //   return (
+  //     <div>
+  //       <MemoList />
+  //     </div>
+  //   );
+  // } else {
+  return (
+    <div className={classNames(styles["container"])}>
+      <Toolbar />
+      <div className={classNames(styles["section-text"])}>
+        <div className={classNames(styles["memo--header"])}>
+          <h2 className={classNames(styles["title"])}>
+            {JSON.parse(localStorage.getItem(id)).title}
+          </h2>
+          <div className={classNames(styles["time"])}>
+            {JSON.parse(localStorage.getItem(id)).time}
           </div>
         </div>
+        <div className={classNames(styles["memo--main-text"])}>
+          {JSON.parse(localStorage.getItem(id)).memo}
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+// }
